bd3c31241e34ebffb8fc05c21eaf95e837341568
CodeShadowing95 <ksenbonsakura@gmail.com>
2024-09-11 14:00:59 +0200

    New commit1

--- components/page-routes/index.ts
+++ components/page-routes/index.ts
@@ -2,12 +2,10 @@ import Dashboard from "./Dashboard";
 import IndexMembers from "./IndexMembers";
 import LocalInfos from "./LocalInfos";
 import Services from "./Services";
-import Videos from "./Videos";
 
 export {
     Dashboard,
     IndexMembers,
     LocalInfos,
     Services,
-    Videos,
 }
